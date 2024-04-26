50f1c13b572cf502513af1f3436dba762361b938
jcopado <jcopado@surlabs.es>
2024-04-26 22:27:14 +0200

    platform/ilias classes

---
+++ classes/platform/ilias/PanoptoException.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Panopto Repository Object plugin for ILIAS.
+ * This plugin allows users to embed Panopto videos in ILIAS as repository objects.
+ *
+ * The Panopto Repository Object plugin for ILIAS is open-source and licensed under GPL-3.0.
+ * For license details, visit https://www.gnu.org/licenses/gpl-3.0.en.html.
+ *
+ * To report bugs or participate in discussions, visit the Mantis system and filter by
+ * the category "Panopto" at https://mantis.ilias.de.
+ *
+ * More information and source code are available at:
+ * https://github.com/surlabs/Panopto
+ *
+ * If you need support, please contact the maintainer of this software at:
+ * info@surlabs.es
+ *
+ */
+
+/**
+ * Class PanoptoException
+ * @authors Jesús Copado, Daniel Cazalla, Saúl Díaz, Juan Aguilar <info@surlabs.es>
+ */
+class PanoptoException extends Exception
+{
+
+}
