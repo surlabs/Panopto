ee406bc2d5a34dd342daccc549e38a0eb18d17c2
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-04-29 16:37:49 +0200

    Análisis y rework de dbupdate

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1,18 +1,38 @@
 <#1>
 <?php
-// Previous Version
+global $DIC;
+$db = $DIC->database();
+if (!$db->tableExists('xpan_settings')) {
+    $fields = array(
+        'obj_id' => array('type' => 'integer', 'length' => 8),
+        'is_online' => array('type' => 'integer', 'length' => 1),
+        'folder_ext_id' => array('type' => 'integer', 'length' => 8)
+    );
+
+    $db->createTable('xpan_settings', $fields);
+    $db->addPrimaryKey('xpan_settings', array('obj_id'));
+}
+if (!$db->tableExists('xpan_config')) {
+    $fields = array(
+        'name' => array('type' => 'text', 'length' => 255),
+        'value' => array('type' => 'text', 'length' => 255)
+    );
+
+    $db->createTable('xpan_config', $fields);
+    $db->addPrimaryKey('xpan_config', array('name'));
+}
 ?>
 <#2>
 <?php
-// Previous Version
+// Unnecessary step because now we will use the language files
 ?>
 <#3>
 <?php
-// Previous Version
+// Unnecessary step because the table rep_robj_srtr_entry always seems to be empty
 ?>
 <#4>
 <?php
-// Previous Version
+// Unnecessary step because all necessary xpan_setting columns have been added in step one and the columns needed for rework or future upgrades will be added from step 5.
 ?>
 <#5>
 <?php
