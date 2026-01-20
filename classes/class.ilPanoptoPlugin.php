3e76ecbb5ad3d221a96f509f9cfaa568985a234e
Daniel Cazalla <86362063+ZallaxDev@users.noreply.github.com>
2026-01-20 12:57:17 +0100

    fix: Fixed plugin directory

--- classes/class.ilPanoptoPlugin.php
+++ classes/class.ilPanoptoPlugin.php
@@ -78,7 +78,7 @@ class ilPanoptoPlugin extends ilRepositoryObjectPlugin
      */
     public function getDirectory(): string
     {
-        return 'public/Customizing/global/plugins/Services/Repository/RepositoryObject/Panopto';
+        return '/Customizing/global/plugins/Services/Repository/RepositoryObject/Panopto';
     }
 
     /**
